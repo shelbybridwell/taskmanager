414ba259e7d88362545e890ef45f1c884626ef8d
Shelby Bridwell <109699848+shelbybridwell@users.noreply.github.com>
2022-09-20 15:01:06 -0500

    working out kinks

--- assets/java.js
+++ assets/java.js
@@ -1,49 +1,51 @@
-var todayDate = moment().format('dddd, MMM Do YYYY');
+todayDate = moment().format('dddd, MMM Do YYYY');
 $("#todaysDate").html(todayDate);
-
-function timeKeeper() {
-    currentTime = moment().hour();
 
 $(document).ready(function () {
         $(".saveBtn").on("click", function () {
             
-            var text = $(this).siblings(".description").val();
-            var time = $(this).parent().attr("id");
+            text = $(this).siblings(".description").val();
+            time = $(this).parent().attr("id");
     
             localStorage.setItem(time, text);
         })
 
+function timeKeeper() {
+    currentTime = moment().hour();
+    $("#todaysTime").html(currentTime);
+
     $(".hour-block").each(function(){
-        blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+
         if (blockHour < currentTime) {
             $(this).removeClass("future");
-            $(this).removeClass("current");
+            $(this).removeClass("present");
             $(this).addClass("past");
         }
         else if (blockHour === currentTime) {
             $(this).removeClass("past");
-            $(this).removeClass("current");
+            $(this).removeClass("future");
             $(this).addClass("present");
         }
         else {
             $(this).removeClass("present");
             $(this).removeClass("past");
-            $(this).addClass("current");
+            $(this).addClass("future");
 
         }
     
     })
 }
-$("#hour8am .description").val(localStorage.getItem("hour8"));
-$("#hour9am .description").val(localStorage.getItem("hour9"));
-$("#hour10am .description").val(localStorage.getItem("hour10"));
-$("#hour11am .description").val(localStorage.getItem("hour11"));
-$("#hour12pm .description").val(localStorage.getItem("hour12"));
-$("#hour13pm .description").val(localStorage.getItem("hour13"));
-$("#hour14pm .description").val(localStorage.getItem("hour14"));
-$("#hour15pm .description").val(localStorage.getItem("hour15"));
-$("#hour16pm .description").val(localStorage.getItem("hour16"));
-$("#hour17pm .description").val(localStorage.getItem("hour17"));
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
 timeKeeper();
 })
